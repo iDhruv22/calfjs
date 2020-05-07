128e499cf3b824efe159731aadcb0915ebe9f1aa
Dhruv Patel <dhruvprotocol@gmail.com>
2020-05-07 12:21:30 +0530

    adding color configs

---
+++ src/configs/colors.js
@@ -0,0 +1,14 @@
+const colors = {
+  primaryGreen: "#27ae60",
+  secondaryGreen: "#2ecc71",
+  primaryBlue: "#2980b9",
+  secondaryBlue: "#3498db",
+  primaryRed: "#c0392b",
+  secondaryRed: "#e74c3c",
+  primarySilver: "#bdc3c7",
+  secondarySilver: "#ecf0f1",
+  primaryGray: "#7f8c8d",
+  secondaryGray: "#95a5a6",
+};
+
+export default colors;
